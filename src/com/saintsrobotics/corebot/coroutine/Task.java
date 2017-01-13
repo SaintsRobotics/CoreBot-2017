00023a4a445560ad47408a4fd56fceb40068f734
Shreyas <skraman1999@gmail.com>
2017-01-13 00:29:51 -0800

    Make waiters fluent

--- src/com/saintsrobotics/corebot/coroutine/Task.java
+++ src/com/saintsrobotics/corebot/coroutine/Task.java
@@ -7,6 +7,23 @@ import java.util.function.BooleanSupplier;
 
 public abstract class Task extends Generator<BooleanSupplier> {
     
+    protected Waiters wait;
+    
     BooleanSupplier waiter = () -> true;
     Iterator<BooleanSupplier> iterator;
+    
+    protected class Waiters {
+        public void forFrame() {
+            yield(() -> true);
+        }
+    
+        public void forSeconds(double secs) {
+            long finalTimeMillis = (long) (secs * 1000) + System.currentTimeMillis();
+            yield(() -> finalTimeMillis < System.currentTimeMillis());
+        }
+    
+        public void until(BooleanSupplier predicate) {
+            yield(predicate);
+        }
+    }
 }
--- src/com/saintsrobotics/corebot/tasks/test/TestMotorsTask.java
+++ src/com/saintsrobotics/corebot/tasks/test/TestMotorsTask.java
@@ -2,7 +2,6 @@ package com.saintsrobotics.corebot.tasks.test;
 
 import com.saintsrobotics.corebot.Robot;
 import com.saintsrobotics.corebot.coroutine.RepeatingTask;
-import com.saintsrobotics.corebot.coroutine.Wait;
 
 public class TestMotorsTask extends RepeatingTask {
     
@@ -11,18 +10,18 @@ public class TestMotorsTask extends RepeatingTask {
         double speed = 0.2;
         
         Robot.motors.leftDrive1.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.leftDrive2.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.leftDrive3.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.rightDrive1.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.rightDrive2.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.rightDrive3.set(speed);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
         Robot.motors.allMotors.set(0);
-        yield(Wait.forSeconds(0.2));
+        wait.forSeconds(0.2);
     }
 }
