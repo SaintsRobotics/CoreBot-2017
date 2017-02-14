fc765b7496e421a9b44304194949c7b764302549
t2001s <tara2001s@gmail.com>
2017-02-13 17:52:16 -0800

    Used robot preferences for PID and Gear Drop angle values

--- src/com/saintsrobotics/corebot/tasks/teleop/GearDropTask.java
+++ src/com/saintsrobotics/corebot/tasks/teleop/GearDropTask.java
@@ -1,29 +1,30 @@
 package com.saintsrobotics.corebot.tasks.teleop;
 
 import com.saintsrobotics.corebot.Robot;
-import com.saintsrobotics.corebot.coroutine.Task;
+import com.saintsrobotics.corebot.coroutine.RunContinuousTask;
 import com.saintsrobotics.corebot.util.PID;
 
-public class GearDropTask extends Task {
-    
-    @Override
-    protected void run() {
-        PID armPid = new PID(0.01, 0, 0);
-        while (true) {
-            wait.until(() -> Robot.oi.drive.buttons.RB());
-            
-            
-            long startTime = System.currentTimeMillis();
-            while (System.currentTimeMillis() < startTime + 500) {
-                double value = armPid.compute(Robot.sensors.potentiometer.get(), 45.0);
-                Robot.motors.gearDrop.set(value);
-                wait.forFrame();
-            }
-            while (System.currentTimeMillis() < startTime + 1000) {
-                double value = armPid.compute(Robot.sensors.potentiometer.get(), 0);
-                Robot.motors.gearDrop.set(value);
-                wait.forFrame();
-            }
+public class GearDropTask extends RunContinuousTask {
+	PID armPid = new PID(Robot.prefs.getDouble("P value", 0.1),
+		Robot.prefs.getDouble("I value", 1.0),
+		Robot.prefs.getDouble("D value", 1.0));
+
+	@Override
+	protected void runContinuously() {
+		wait.until(() -> Robot.oi.drive.buttons.RB());
+        long startTime = System.currentTimeMillis();
+        
+        while (System.currentTimeMillis() < startTime + 500) {
+            double value = armPid.compute(Robot.sensors.potentiometer.get(), Robot.prefs.getDouble("Top Gear Drop Angle", 45.0));
+            Robot.motors.gearDrop.set(value);
+            wait.forFrame();
         }
-    }
+        
+        while (System.currentTimeMillis() < startTime + 1000) {
+            double value = armPid.compute(Robot.sensors.potentiometer.get(), Robot.prefs.getDouble("Bottom Gear Drop Angle", 0));
+            Robot.motors.gearDrop.set(value);
+            wait.forFrame();
+        }
+
+	}
 }
