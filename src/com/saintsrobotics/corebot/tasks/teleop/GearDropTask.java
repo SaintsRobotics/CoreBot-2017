0db9ae2e1bd65c36e0d4be2628e6fb04b5df68a4
Shreyas <skraman1999@gmail.com>
2017-02-19 18:43:16 -0800

    Improve gear drop logic

--- src/com/saintsrobotics/corebot/tasks/teleop/GearDropTask.java
+++ src/com/saintsrobotics/corebot/tasks/teleop/GearDropTask.java
@@ -11,34 +11,33 @@ public class GearDropTask extends RunContinuousTask {
         Robot.prefs.getDouble("I value", 0),
         Robot.prefs.getDouble("D value", 0));
 
+    private double gearDropOut = Robot.prefs.getDouble("geardrop_out", -1);
+    private double gearDropIn = Robot.prefs.getDouble("geardrop_in", -1);
+        
     @Override
     protected void runContinuously() {
-        wait.until(() -> Robot.oi.drive.buttons.RB());
-        
-        double gearDropOut = Robot.prefs.getDouble("geardrop_out", -1);
-        double gearDropIn = Robot.prefs.getDouble("geardrop_in", -1);
-        
-        if (gearDropOut != -1 && gearDropIn != -1) {
-            long startTime = System.currentTimeMillis();
+        while (!Robot.oi.drive.buttons.RB()) {
+            double value = -armPid.compute(Robot.sensors.potentiometer.get(), gearDropIn);
+            SmartDashboard.putNumber("geardrop_in_motor", value);
+            Robot.motors.gearDrop.set(Math.signum(value)*Math.min(Math.abs(value), 0.2));
+            wait.forFrame();
+        }
     
+        if (gearDropOut != -1 && gearDropIn != -1) {
+            
+            long startTime = System.currentTimeMillis();
+            
             while (System.currentTimeMillis() < startTime + 2000) {
                 double value = -armPid.compute(Robot.sensors.potentiometer.get(), gearDropOut);
                 SmartDashboard.putNumber("geardrop_out_motor", value);
-                Robot.motors.gearDrop.set(Math.signum(value)*Math.min(Math.abs(value), 1.0));
+                Robot.motors.gearDrop.set(Math.signum(value) * Math.min(Math.abs(value), 1.0));
                 wait.forFrame();
             }
     
             while (System.currentTimeMillis() < startTime + 4000) {
                 double value = -armPid.compute(Robot.sensors.potentiometer.get(), gearDropIn);
                 SmartDashboard.putNumber("geardrop_in_motor", value);
-                Robot.motors.gearDrop.set(Math.signum(value)*Math.min(Math.abs(value), 1.0));
-                wait.forFrame();
-            }
-            
-            while (!Robot.oi.drive.buttons.RB()) {
-                double value = -armPid.compute(Robot.sensors.potentiometer.get(), gearDropIn);
-                SmartDashboard.putNumber("geardrop_in_motor", value);
-                Robot.motors.gearDrop.set(Math.signum(value)*Math.min(Math.abs(value), 0.2));
+                Robot.motors.gearDrop.set(Math.signum(value) * Math.min(Math.abs(value), 1.0));
                 wait.forFrame();
             }
         }
