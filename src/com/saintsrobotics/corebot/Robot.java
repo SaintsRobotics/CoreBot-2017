df0342c404a964fc2151efb32bb971e02cf3ec94
Shreyas <skraman1999@gmail.com>
2017-02-21 13:15:47 -0800

    Minor code cleanup

--- src/com/saintsrobotics/corebot/Robot.java
+++ src/com/saintsrobotics/corebot/Robot.java
@@ -3,20 +3,12 @@ package com.saintsrobotics.corebot;
 import com.saintsrobotics.corebot.coroutine.RunEachFrameTask;
 import com.saintsrobotics.corebot.coroutine.Task;
 import com.saintsrobotics.corebot.coroutine.TaskRunner;
-import com.saintsrobotics.corebot.input.CompetitionSensors;
 import com.saintsrobotics.corebot.input.OI;
+import com.saintsrobotics.corebot.input.PracticeSensors;
 import com.saintsrobotics.corebot.input.Sensors;
-import com.saintsrobotics.corebot.output.CompetitionBotMotors;
-import com.saintsrobotics.corebot.output.CompetitionBotServos;
-import com.saintsrobotics.corebot.output.Motors;
-import com.saintsrobotics.corebot.output.Servos;
+import com.saintsrobotics.corebot.output.*;
 import com.saintsrobotics.corebot.tasks.UpdateMotors;
-import com.saintsrobotics.corebot.tasks.autonomous.CenterTargetAutonRightTask;
-import com.saintsrobotics.corebot.tasks.autonomous.CenterTargetLeftAutonTask;
-import com.saintsrobotics.corebot.tasks.autonomous.DriveStraightAutonTask;
-import com.saintsrobotics.corebot.tasks.autonomous.LeftTargetAutonTask;
-import com.saintsrobotics.corebot.tasks.autonomous.RightTargetAutonTask;
-import com.saintsrobotics.corebot.tasks.autonomous.TurnToFaceVisionTargetTask;
+import com.saintsrobotics.corebot.tasks.autonomous.*;
 import com.saintsrobotics.corebot.tasks.teleop.ArcadeDriveTask;
 import com.saintsrobotics.corebot.tasks.teleop.GearDropTask;
 import com.saintsrobotics.corebot.tasks.teleop.LifterTask;
@@ -25,7 +17,6 @@ import com.saintsrobotics.corebot.tasks.test.TestGearDropTask;
 import com.saintsrobotics.corebot.tasks.test.TestLEDTask;
 import com.saintsrobotics.corebot.tasks.test.TestMotorsTask;
 import com.saintsrobotics.corebot.tasks.test.TestShifterTask;
-
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -44,9 +35,9 @@ public class Robot extends IterativeRobot {
     public static NetworkTable visionTable;
     public static Preferences prefs;
 
-    public static Sensors sensors = new CompetitionSensors();
-    public static Motors motors = new CompetitionBotMotors();
-    public static Servos servos = new CompetitionBotServos();
+    public static Sensors sensors = new PracticeSensors();
+    public static Motors motors = new PracticeBotMotors();
+    public static Servos servos = new PracticeBotServos();
     public static OI oi = new OI();
 
     private UsbCamera camera;
@@ -74,9 +65,9 @@ public class Robot extends IterativeRobot {
         }).start();
         visionTable = NetworkTable.getTable("/GRIP/myContoursReport");
         
+        taskChooser.addDefault("RightTargetAutonTask", RightTargetAutonTask::new);
         taskChooser.addObject("DriveStraightTask", DriveStraightAutonTask::new);
         taskChooser.addObject("TestMotorsTask", TestMotorsTask::new);
-        taskChooser.addDefault("RightTargetAutonTask", RightTargetAutonTask::new);
         taskChooser.addObject("CenterTargetAutonRightTask", CenterTargetAutonRightTask::new);
         taskChooser.addObject("CenterTargetLeftAutonTask", CenterTargetLeftAutonTask::new);
         taskChooser.addObject("TurnToFaceVisionTargetTask", TurnToFaceVisionTargetTask::new);
